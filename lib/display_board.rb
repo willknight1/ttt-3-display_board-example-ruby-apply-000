65019e169ff2b54187818443873cf14bfa25624e
William Knight <wknight1027@gmail.com>
2020-08-10 19:50:54 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,7 @@
 def display_board
 phrase = "tic tac toe"
-puts "   |   |    "
-puts "------------"
-puts "   |   |    "
-puts "------------"
-puts "   |   |    "
- end
+   |   |   
+-----------
+   |   |   
+-----------
+   |   |   
